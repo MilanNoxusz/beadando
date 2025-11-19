67310d1533906c00ce17c21ad0af32ae4dd4acc7
saribence <saribence10@gmail.com>
2025-11-19 11:05:20 +0100

    fooldal alap megcsinalva.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 		<title>Napfény Tours - Ahol az utazás a lélekhez ér</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="{{ asset('assets/css/main.css') }}" />
 		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
 	</head>
 	<body class="is-preload">
@@ -174,12 +174,12 @@
 
 			</div>
 
-		<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/jquery.scrollex.min.js"></script>
-			<script src="assets/js/browser.min.js"></script>
-			<script src="assets/js/breakpoints.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<script src="assets/js/main.js"></script>
+		<script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+			<script src="{{asset('assets/js/jquery.scrollex.min.js')}}"></script>
+			<script src="{{asset('assets/js/browser.min.js')}}"></script>
+			<script src="{{asset('assets/js/breakpoints.min.js')}}"></script>
+			<script src="{{asset('assets/js/util.js')}}"></script>
+			<script src="{{asset('assets/js/main.js')}}"></script>
 
 	</body>
 </html>
