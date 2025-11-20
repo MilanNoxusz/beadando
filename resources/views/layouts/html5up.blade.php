a326ca0304106215f0063a96d2f8e9759032799a
saribence <saribence10@gmail.com>
2025-11-20 19:31:28 +0100

    Menujavítás

--- resources/views/layouts/html5up.blade.php
+++ resources/views/layouts/html5up.blade.php
@@ -6,6 +6,24 @@
     <title>@yield('title', config('app.name'))</title>
     <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}" />
     <noscript><link rel="stylesheet" href="{{ asset('assets/css/noscript.css') }}" /></noscript>
+    <style>
+        input[type="text"], input[type="password"], input[type="email"], 
+        input[type="tel"], input[type="number"], input[type="date"], 
+        select, textarea {
+            background-color: #ffffff !important;
+            color: #222222 !important;
+            font-weight: 600;
+        }
+        
+        ::placeholder {
+            color: #777777 !important;
+            opacity: 1;
+        }
+        
+        select {
+            background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='40' height='40' preserveAspectRatio='none' viewBox='0 0 40 40'%3E%3Cpath d='M9.4,12.3l10.4,10.4l10.4-10.4c0.2-0.2,0.5-0.4,0.9-0.4c0.3,0,0.6,0.1,0.9,0.4l3.3,3.3c0.2,0.2,0.4,0.5,0.4,0.9 c0,0.4-0.1,0.6-0.4,0.9L20.7,31.9c-0.2,0.2-0.5,0.4-0.9,0.4c-0.3,0-0.6-0.1-0.9-0.4L4.3,17.3c-0.2-0.2-0.4-0.5-0.4-0.9 c0-0.4,0.1-0.6,0.4-0.9l3.3-3.3c0.2-0.2,0.5-0.4,0.9-0.4S9.1,12.1,9.4,12.3z' fill='%23222222' /%3E%3C/svg%3E") !important;
+        }
+    </style>
     @stack('head')
 </head>
 <body class="@yield('body_class', 'is-preload')">
